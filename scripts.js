407f7df9496583100d5ce5accc9305eb93ee756d
ericsallinger <eric.sallinger303@gmail.com>
2023-04-30 12:26:26 -0600

    change play/pause to css based

--- scripts.js
+++ scripts.js
@@ -1,14 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
   const audioPlayer = document.getElementById("audio-player");
   const playButton = document.getElementById("play-button");
+  const playIcon = playButton.querySelector(".play-icon");
+  const pauseIcon = playButton.querySelector(".pause-icon");
 
   playButton.addEventListener("click", function () {
     if (audioPlayer.paused) {
       audioPlayer.play();
-      playButton.innerHTML = "&#10074;&#10074;";
+      playIcon.style.display = "none";
+      pauseIcon.style.display = "block";
     } else {
       audioPlayer.pause();
-      playButton.innerHTML = "&#9658;";
+      playIcon.style.display = "block";
+      pauseIcon.style.display = "none";
     }
   });
 });
